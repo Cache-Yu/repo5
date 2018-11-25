1b8f987e602a6d578061c70f9e796cc88440dc44
xaio <xaio@itcast.com>
2018-11-25 19:06:33 +0800

    添加工程

---
+++ git-first/src/com/itcast/GitFirst.java
@@ -0,0 +1,11 @@
+package com.itcast;
+
+public class GitFirst {
+    public static void main(String[] args) {
+        System.out.println("hello Git ");
+
+
+        System.out.println("hello Git ");
+        System.out.println("hello Git ");
+    }
+}
