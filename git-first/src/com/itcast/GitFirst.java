f920f9360c771bed7d36927f45319cb9daece8a2
xaio <xaio@itcast.com>
2018-11-25 19:11:47 +0800

    修改了其中的一些代码！！

--- git-first/src/com/itcast/GitFirst.java
+++ git-first/src/com/itcast/GitFirst.java
@@ -4,7 +4,7 @@ public class GitFirst {
     public static void main(String[] args) {
         System.out.println("hello Git ");
 
-
+        System.out.println("修啊代码");
         System.out.println("hello Git ");
         System.out.println("hello Git ");
     }
